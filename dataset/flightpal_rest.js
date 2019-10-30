96530c98d041218e3ac1434ef5c861f530e26214
fanos <smelaku@gmail.com>
2019-10-29 22:02:48 -0400

    Addd index.html and added sql query link from client to server.

--- dataset/flightpal_rest.js
+++ dataset/flightpal_rest.js
@@ -3,24 +3,58 @@ var db = new sqlite3.Database('flightpal.db');
 
 var express = require('express');
 var restapi = express();
+var bodyParser = require('body-parser');
+//restapi.use(bodyParser.json());
+restapi.use(bodyParser.text());
+//restapi.use(express.bodyParser());
 
 restapi.get('/test', function(req, res){
     res.json({message: 'Hello! from flightpal rest api'})
 });
 
-restapi.get('/flights_time', function(req, res){
-    var firstSeen = req.query.firstseen;
-    var lastSeen = req.query.lastseen;
+// restapi.get('/flights_time', function(req, res){
+//     var firstSeen = req.query.firstseen;
+//     var lastSeen = req.query.lastseen;
 
-    if (firstSeen != undefined && lastSeen != undefined)
+//     if (firstSeen != undefined && lastSeen != undefined)
+//     {
+//         var queryString = "SELECT COUNT(*) FROM opensky WHERE firstseen >= " + firstSeen + " AND lastseen <= " + lastSeen;
+
+//         db.all(queryString, function(err, row){
+//             if (err)
+//             {
+//                 res.sendStatus(500);
+//                 console.log("Query returned error ", err);
+//             }
+//             else
+//             {
+//                 console.log(row);
+//                 res.json(row);
+//             }
+//         });
+//         console.log(queryString, firstSeen, lastSeen);
+//     }
+//     else
+//     {
+//         res.status(404);
+//         res.end();
+//     }
+// });
+
+restapi.post('/sqlquery', function(req, res){
+    queryString = req.body;
+    console.log("/sqlquery\n" , queryString);
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST");
+
+    if(queryString != undefined)
     {
-        var queryString = "SELECT * FROM opensky WHERE firstseen >= " + firstSeen + " AND lastseen <= " + lastSeen;
-
-        db.all(queryString, function(err, row){
+        db.all(queryString, function(err, row)
+        {
             if (err)
             {
                 res.sendStatus(500);
-                console.log("Query returned error ", err);
+                console.log("query returned error ", err)
             }
             else
             {
@@ -28,12 +62,11 @@ restapi.get('/flights_time', function(req, res){
                 res.json(row);
             }
         });
-        console.log(queryString, firstSeen, lastSeen);
     }
     else
     {
-        res.status(404);
-        res.end();
+        res.sendStatus(404);
     }
 });
+
 restapi.listen(8000);
